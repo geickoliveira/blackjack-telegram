8914a79bea3596b8cf2e43416862f326eccc493a
Geickson Oliveira <geickson2@gmail.com>
2025-03-20 02:10:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -247,7 +247,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function showAd() {
-        show_9108066('pop').then(() => {
+        show_9108066().then(() => {
             // Adiciona créditos extras após o anúncio
             balance += 10;
             balanceValue.innerText = balance;
